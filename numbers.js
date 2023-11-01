cb26850f3776763f04b5904c3dc2b9dd0da80928
Евгений Константинов <kel.7@mail.ru>
2023-11-01 15:25:13 +0300

    nomer1

--- numbers.js
+++ numbers.js
@@ -1,1 +1,23 @@
+// 1 задача //
+export const calculateDistance = (x1, x2) => {
+    return Math.abs(x1 - x2);
+};
+// 2 задача //
+export const calculateSegmentProduct = () => {}
+// 3 задача //
+export const calculateKilobytes = () => {}
+// 4 задача //
+export const calculateSegments = () => {}
+// 5 задача //
+export const calculateDigitSum = () => {}
+// 6 задача //
+export const swapHundredsAndTens = () => {}
+// 7 задача //
+export const getHundredsDigit = () => {}
+// 8 задача //
+export const getFullHours = () => {}
+// 9 задача //
+export const getDayOfWeek = () => {}
+// 10 задача //
+export const countSquares = () => {}
 
